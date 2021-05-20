65c6d039aab2c8598fcdeccd19ca685b361313ee
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-20 21:27:54 +0300

    banner.js create

---
+++ banner.js
@@ -0,0 +1,4 @@
+// 13N sukurti atskirame faile funkcija banner, kuri grazina i html
+// sustilizuota banneri. css galite aprasyti css atskiram faile.
+// banneryje yra antrasta, paraste, mygtukas,
+// extra, paspaudus mygtuka padaryti console log
